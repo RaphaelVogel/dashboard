ad54692e3191ff0b9115cd587bf0e7eac324e859
Raphael Vogel <raphael.vogel@web.de>
2015-04-02 22:17:28 +0200

    change root

--- server.py
+++ server.py
@@ -8,12 +8,12 @@ import web.routes
 # logger configuration
 logger = logging.getLogger("dash_logger")
 logger.setLevel(logging.WARN)
-filehandler = RotatingFileHandler('./log.txt', maxBytes=100000, backupCount=3)
+filehandler = RotatingFileHandler('./dashboard/log.txt', maxBytes=100000, backupCount=3)
 formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
 filehandler.setFormatter(formatter)
 logger.addHandler(filehandler)
 
-TEMPLATE_PATH.insert(0, './web/views')
+TEMPLATE_PATH.insert(0, './dashboard/web/views')
 
 if len(sys.argv) > 1 and sys.argv[1] == 'devmode':
     run(server='cherrypy', host='localhost', port=8080, debug=True, reloader=True)
