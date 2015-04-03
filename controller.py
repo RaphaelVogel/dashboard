a2dedabc457e46f43ddc58714c1c1a315b76aa44
Raphael Vogel <raphael.vogel@web.de>
2015-04-03 19:49:40 +0200

    threading

--- controller.py
+++ controller.py
@@ -30,6 +30,16 @@ EVENT_WAIT_SLEEP_SECONDS = 0.1
 
 
 # --- Start thread to turn off monitor ------------------------------------------------------------------------------
+def turn_monitor_off():
+    while True:
+        time.sleep(30)
+        status = monitor.status()
+        if status == "ON":
+            monitor.switch_off()
+
+
+thread = Thread(target=turn_monitor_off)
+thread.run()
 
 
 # --- Define functions to call if button is touched ------------------------------------------------------------------
