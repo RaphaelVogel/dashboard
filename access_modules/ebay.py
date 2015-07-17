2cdaeb65a2d682168be24be8a52ef0f0a5d71d8d
Raphael Vogel <raphael.vogel@web.de>
2015-07-17 21:15:46 +0200

    fix ebay - change in HTML

--- access_modules/ebay.py
+++ access_modules/ebay.py
@@ -11,18 +11,18 @@ def get_data():
 
     soup = BeautifulSoup(resp.text)
     sections = soup.find_all("section")
-    return_data = []
+    return_data = []    # List of dictionaries like {image_url: "http://...", text: "Verkaufe....", price: "10"}
     for section in sections:
         if isinstance(section, NavigableString):
             continue
-        if section.get('class') and section['class'][0] == "ad-listitem-image":
-            if section.a.get('data-imgsrc'):
-                entry = {'image_url': section.a['data-imgsrc']}
+        if section.get('class') and section['class'][0] == "rbr-aditem-image":
+            if section.div.get('data-imgsrc'):
+                entry = {'image_url': section.div['data-imgsrc']}
             else:
                 entry = {'image_url': '/lib/images/nopic.png'}
-        elif section.get('class') and section['class'][0] == "ad-listitem-main":
+        elif section.get('class') and section['class'][0] == "rbr-aditem-main":
             entry['text'] = section.h2.a.string
-        elif section.get('class') and section['class'][0] == "ad-listitem-details":
+        elif section.get('class') and section['class'][0] == "rbr-aditem-details":
             entry['price'] = section.strong.string
             return_data.append(entry)
 
