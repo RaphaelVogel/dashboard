722292b3560200018ad2938849909226d5817979
RaphaelVogel <raphael.vogel@icloud.com>
2016-02-07 20:06:54 +0100

    set loglevel to WARN

--- controller.py
+++ controller.py
@@ -10,7 +10,7 @@ import Adafruit_MPR121.MPR121 as MPR121
 import RPi.GPIO as GPIO
 
 logger = logging.getLogger("controller_logger")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.WARN)
 filehandler = RotatingFileHandler('./dashboard/log_controller.txt', maxBytes=100000, backupCount=3)
 formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
 filehandler.setFormatter(formatter)
