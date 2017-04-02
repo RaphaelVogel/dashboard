9b982ef4254eff2376f456b187c1447c1f34cd88
RaphaelVogel <raphael.vogel@icloud.com>
2017-04-02 12:27:09 +0200

    fix

--- access_modules/camera.py
+++ access_modules/camera.py
@@ -7,4 +7,4 @@ config.read('/home/pi/dashboard/tools/config.txt')
 def get_camera_data(number):
     camera_name = config.get('cam' + str(number), 'name')
     camera_url = config.get('cam' + str(number), 'url')
-    return {name: camera_name, url: camera_url}
+    return {'name': camera_name, 'url': camera_url}
