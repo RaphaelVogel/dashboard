4485d8e2a84731c39fca97e59c02417ece5eb4aa
RaphaelVogel <raphael.vogel@icloud.com>
2017-04-02 12:19:41 +0200

    fix

--- access_modules/camera.py
+++ access_modules/camera.py
@@ -1,11 +1,10 @@
 import ConfigParser
-import requests
 
 config = ConfigParser.RawConfigParser()
 config.read('/home/pi/dashboard/tools/config.txt')
 
 
 def get_camera_data(number):
-    camera_name = config.get('cam' + number, 'name')
-    camera_url = config.get('cam' + number, 'url')
+    camera_name = config.get('cam' + str(number), 'name')
+    camera_url = config.get('cam' + str(number), 'url')
     return {name: camera_name, url: camera_url}
