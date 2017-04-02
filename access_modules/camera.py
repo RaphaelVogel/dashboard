003601b0044ea1bc1a90444ea535642730a6d5a7
RaphaelVogel <raphael.vogel@icloud.com>
2017-04-02 12:07:44 +0200

    fix

--- access_modules/camera.py
+++ access_modules/camera.py
@@ -5,7 +5,7 @@ config = ConfigParser.RawConfigParser()
 config.read('/home/pi/dashboard/tools/config.txt')
 
 
-def activate_camera(number):
+def get_camera_data(number):
     camera_name = config.get('cam' + number, 'name')
     camera_url = config.get('cam' + number, 'url')
     return {name: camera_name, url: camera_url}
