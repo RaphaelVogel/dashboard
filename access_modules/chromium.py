e21f2c8b1066d31230272a5ba3936893f811dde9
RaphaelVogel <raphael.vogel@icloud.com>
2017-10-03 22:43:11 +0200

    change pychrome usage

--- access_modules/chromium.py
+++ access_modules/chromium.py
@@ -2,11 +2,10 @@ import pychrome
 
 browser = pychrome.Browser(url="http://127.0.0.1:9222")
 tab = browser.new_tab()
+tab.start()
+tab.Network.enable()
 
 
-def open_url(url):
-    tab.start()
-    tab.Network.enable()
-    tab.Page.navigate(url="https://github.com/fate0/pychrome", _timeout=8)
-    tab.wait(8)
-    tab.stop()
+def open_url(newurl):
+    tab.Page.navigate(url=newurl, _timeout=5)
+    tab.wait(5)
