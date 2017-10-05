4493c16bf90c019b506e70ad2e51bc0bbeabaa45
RaphaelVogel <raphael.vogel@icloud.com>
2017-10-05 21:57:46 +0200

    change to Chromote

--- access_modules/chromium.py
+++ access_modules/chromium.py
@@ -1,11 +1,7 @@
-import pychrome
-
-browser = pychrome.Browser(url="http://127.0.0.1:9222")
-tab = browser.new_tab()
-tab.start()
-tab.Network.enable()
+from chromote import Chromote
 
 
-def open_url(newurl):
-    tab.Page.navigate(url=newurl, _timeout=5)
-    tab.wait(5)
+def open_url(url):
+    chrome = Chromote()
+    tab = chrome.tabs[0]
+    tab.set_url(url)
