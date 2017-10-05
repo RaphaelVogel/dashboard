5fcf3783231dadd26fb22a5eb76e905fc0d9b49b
RaphaelVogel <raphael.vogel@icloud.com>
2017-10-05 22:20:11 +0200

    remote sudo from monitor switch

--- access_modules/monitor.py
+++ access_modules/monitor.py
@@ -7,15 +7,15 @@ TIMER_START = 210.0
 
 
 def switch_off():
-    subprocess.call("sudo /opt/vc/bin/tvservice -o", shell=True)
+    subprocess.call("/opt/vc/bin/tvservice -o", shell=True)
 
 
 def switch_on():
-    subprocess.call("sudo /opt/vc/bin/tvservice -p; sudo chvt 6; sudo chvt 7", shell=True)
+    subprocess.call("/opt/vc/bin/tvservice -p; sudo chvt 6; sudo chvt 7", shell=True)
 
 
 def status():
-    out = subprocess.check_output("sudo /opt/vc/bin/tvservice -s", shell=True)
+    out = subprocess.check_output("/opt/vc/bin/tvservice -s", shell=True)
     if "TV is off" in out:
         return "OFF"
     else:
