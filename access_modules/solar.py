568e9fad4edbce4eba42c5eb45b679f8249a96dd
RaphaelVogel <raphael.vogel@icloud.com>
2020-02-15 12:26:30 +0100

    add solar data

---
+++ access_modules/solar.py
@@ -0,0 +1,47 @@
+import logging
+import requests
+from access_modules import cfg
+from requests.auth import HTTPDigestAuth
+
+
+logger = logging.getLogger("dashboard_logger")
+
+
+def read_data():
+    try:
+        resp = requests.get(cfg['solar']['url'], headers={'Accept': '*/*'},
+                            auth=HTTPDigestAuth(cfg['solar']['user'], cfg['solar']['password']), timeout=2.5)
+    except Exception as e:
+        logger.info('Could not read data from solar inverter: %s' % str(e))
+        return
+
+    logger.debug('Response from solar inverter %s' % resp.text)
+    # example body
+    # master;5.47 kW;5.47 kVA;0.00 kvar;7.05 kWh;7.05 kVAh;0.00 kvarh;34.54 kWh;34.54 kVAh;0.00 kvarh;10.10 MWh;
+    # 10.10 MVAh;0.00 Mvarh;31.10 MWh;31.10 MVAh;0.00 Mvarh;
+    # 1;AT 5000;2.91 kW;4.1 kWh;16.44 MWh;0055A1701029;268435492;3;00100401;0
+    # 2;NT 4200;2.53 kW;3.4 kWh;14.66 MWh;0044A0313104;268435492;3;00200402;0
+    split_content = resp.text.split(';')
+    if split_content[0] != 'master':
+        logger.debug('Incorrect data format from solar inverter')
+        return
+
+    solar_data = dict()
+    current_data = split_content[1].split()
+    solar_data['current'] = current_data[0]
+    solar_data['current_unit'] = current_data[1]
+    day_data = split_content[4].split()
+    solar_data['day'] = day_data[0]
+    solar_data['day_unit'] = day_data[1]
+    month_data = split_content[7].split()
+    solar_data['month'] = month_data[0]
+    solar_data['month_unit'] = month_data[1]
+    year_data = split_content[10].split()
+    solar_data['year'] = year_data[0]
+    solar_data['year_unit'] = year_data[1]
+    total_data = split_content[13].split()
+    solar_data['total'] = total_data[0]
+    solar_data['total_unit'] = total_data[1]
+
+    logger.debug('Dictionary values created from solar inverter %s' % str(solar_data))
+    return solar_data
