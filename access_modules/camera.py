dfe1e5e054ced5b703f5032ed6ed250ca06e3af7
RaphaelVogel <raphael.vogel@icloud.com>
2020-02-22 19:10:23 +0100

    add lock to camera handling

--- access_modules/camera.py
+++ access_modules/camera.py
@@ -1,16 +1,18 @@
 from omxplayer.player import OMXPlayer  # pylint: disable=import-error
-from threading import Timer
+from threading import Timer, Lock
 from access_modules import cfg, monitor
 
 
 g_player = None
 g_timer = None
 CAMERA_ON_TIME = 80.0
+lock = Lock()
 
 
 def display_camera_data(number):
     camera_url = cfg['cam' + str(number)]['url']
     global g_player, g_timer
+    lock.acquire()
     if not g_player:
         # first call
         g_player = OMXPlayer(camera_url)
@@ -27,7 +29,7 @@ def display_camera_data(number):
         g_timer = Timer(CAMERA_ON_TIME, quit_camera)
         g_timer.start()
         monitor.reset_timer()
-
+    lock.release()
 
 def quit_camera():
     global g_player, g_timer
